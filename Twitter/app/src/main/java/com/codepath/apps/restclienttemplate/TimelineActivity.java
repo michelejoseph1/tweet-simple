5b6abb590686370b9523d861cf8fc4a93a67a1ed
Michele Allison Joseph <michelejoseph@fb.com>
2022-06-07 14:10:10 -0700

    added logout functionality

--- Twitter/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ Twitter/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,6 +9,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -27,6 +32,7 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetAdapter adapter;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,7 @@ public class TimelineActivity extends AppCompatActivity {
 
         //find the recycler view
         rvTweets = findViewById(R.id.rvTweets);
+        logoutButton = findViewById(R.id.logoutButton);
         //init the list of tweets and adapter
         tweets = new ArrayList<>();
         adapter = new TweetAdapter(this, tweets);
@@ -44,8 +51,30 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(adapter);
         Log.d(TAG, " In the onCreate");
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLogoutButton();
+            }
+        });
         populateHomeTimeline();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.compose) {
+return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() {
@@ -70,9 +99,11 @@ public class TimelineActivity extends AppCompatActivity {
     }
 
 
+
         void onLogoutButton() {
             TwitterApp.getRestClient(this).clearAccessToken();
             Intent i = new Intent(this, LoginActivity.class);
+            finish();
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(i);
